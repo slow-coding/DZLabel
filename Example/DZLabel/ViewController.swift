a1adcf16bef4fbc356bee3bdb630eacada6427b0
Darren Zheng <623767307@qq.com>
2018-09-27 16:26:58 +0800

    init

--- Example/DZLabel/ViewController.swift
+++ Example/DZLabel/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
-
+import DZLabel
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let l = DZLabel()
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
